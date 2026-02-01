f303b582809105257f5e0af30cdbd421fc486afa
Mike-2002-mx <mscarlxrdm@gmail.com>
2026-01-31 23:16:11 -0600

    First problem to try pull Request

---
+++ HackerRankJava/src/main/java/org/example/java/WelcomeJava.java
@@ -0,0 +1,13 @@
+/*
+Ejercicio sacado de
+https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+Welcome to java
+*/
+package org.example.java;
+
+public class WelcomeJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
